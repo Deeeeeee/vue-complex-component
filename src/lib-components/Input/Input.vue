6b221d6324413fdcda09a632c54c9c2e1cc8e6f8
wangjieyee <wangjieyee@163.com>
2019-06-26 11:54:09 +0800

    baseInput slot文档说明

--- src/lib-components/Input/Input.vue
+++ src/lib-components/Input/Input.vue
@@ -3,14 +3,18 @@
             v-on="$listeners"
             @blur="blur">
     <template slot='prefix'>
+      <!-- @slot 输入框头部内容，只对 type="text" 有效 -->
       <slot name='prefix'></slot>
     </template>
     <template slot='suffix'>
+      <!-- @slot 输入框尾部内容，只对 type="text" 有效 -->
       <slot name='suffix'></slot>
     </template> <template slot='prepend'>
+      <!-- @slot 输入框前置内容，只对 type="text" 有效 -->
       <slot name='prepend'></slot>
     </template>
     <template slot='append'>
+      <!-- @slot 输入框后置内容，只对 type="text" 有效 -->
       <slot name='append'></slot>
     </template>
   </el-input>
